dc83bf46cf677b9a3c8c661d94af8c87ab8a58aa
alex chika <mcluckey1@gmail.com>
2022-06-21 03:21:13 +0100

    updated : app complete

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,10 +3,12 @@ import { getFirestore, collection, doc } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyC5eGHiAVf4MoghkX61DPjcmpMwBl0v2x8",
   authDomain: "working-with-firebase-b8225.firebaseapp.com",
+  databaseURL:
+    "https://working-with-firebase-b8225-default-rtdb.firebaseio.com",
   projectId: "working-with-firebase-b8225",
   storageBucket: "working-with-firebase-b8225.appspot.com",
   messagingSenderId: "481592456079",
-  appId: "1:481592456079:web:0bf7e8130b80140f2391fa",
+  appId: "1:481592456079:web:e6d9e406d7e5780f2391fa",
 };
 
 // Initialize Firebase
